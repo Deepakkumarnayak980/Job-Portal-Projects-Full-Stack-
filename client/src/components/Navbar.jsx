603ccbf545496adbe1f19f2b2337e1089c685a40
Deepakkumarnayak980 <deepakkumarnayak715@gmail.com>
2025-01-03 23:34:08 +0530

    add the applyJobs  page and Application pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { assets } from '../assets/assets';
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
   const { openSignIn } = useClerk(); // Ensure this is a function call
   const { user } = useUser();
 
+  const navigate =useNavigate()
+
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
-        <img src={assets.logo} alt="Logo" />
+        <img onClick={() => navigate('/')} className='cursor-pointer' src={assets.logo} alt="Logo" />
         {
             user
             ?<div className='flex items-center gap-3'>
--- client/src/pages/Application.jsx
+++ client/src/pages/Application.jsx
@@ -1,11 +1,35 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar'
 
 const Application = () => {
+
+  const [isEdit,setIsEdit]=useState(false)
+
   return (
-    <div>
-      Application
-    </div>
+    <>
+      <Navbar />
+      <div className='container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10'>
+        <h2 className='text-xl font-semibold'>Your Resume</h2>
+        <div className='flex gap-2 mb-6 mt-3'>
+           {
+            isEdit ?
+            <>
+            
+            </>
+            :
+            <div className='flex gap-2 '>
+              <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg' href=''>
+                Resume
+              </a>
+              <button className='text-gray-600 border border-gray-300 rounded-lg px-4 py-2'>
+                Edit
+              </button>
+            </div>
+           }
+        </div>
+      </div>
+    </>
   )
 }
 

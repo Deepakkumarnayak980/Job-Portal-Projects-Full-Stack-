5d7d253e225e49e3b8421dec9854877cb819dba8
Deepakkumarnayak980 <deepakkumarnayak715@gmail.com>
2025-01-06 22:13:25 +0530

    add the Dashboard page and styling this page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,27 @@ import ApplyJob from './pages/ApplyJob'
 import Application from './pages/Application'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
 
 const App = () => {
- 
-  const {showRecruiterLogin}=useContext(AppContext)
+
+  const { showRecruiterLogin } = useContext(AppContext)
 
   return (
     <div>
       {showRecruiterLogin && <RecruiterLogin />}
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/apply-job/:id' element={<ApplyJob/>} />
-        <Route path='/Application' element={<Application/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/application" element={<Application />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="view-application" element={<ViewApplications />} />
+        </Route>
       </Routes>
     </div>
   )
